fb4048d07cb64a7ce4a7b6b5d950b8aeed2e827a
ambrozik <hug.a@hotmail.fr>
2021-12-01 10:38:47 +0100

    added findPostsPublicAndById  with token

--- src/posts/validators/handler-publisher.ts
+++ src/posts/validators/handler-publisher.ts
@@ -3,7 +3,7 @@ import { IsMongoId, IsNotEmpty } from 'class-validator';
 export class HandlerPublisher {
   @IsMongoId()
   @IsNotEmpty({
-    message: 'pseudo must not be empty',
+    message: 'publisherId must not be empty',
   })
   publisherId: string;
 }
--- src/posts/validators/handler-publisherId-and-null.ts
+++
@@ -1,7 +0,0 @@
-import { IsMongoId, IsNotEmpty } from 'class-validator';
-
-export class HandlerPublisherIdAndNull {
-  @IsMongoId()
-  @IsNotEmpty()
-  publisherId: string;
-}
